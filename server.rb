fe74f0a4784420779b365e2b32cc622a99c5bcb6
Sergio Morales <sergio.morales@crowdint.com>
2013-10-23 12:59:42 -0500

    First prototype of the server

---
+++ server.rb
@@ -0,0 +1,55 @@
+require 'socket'
+require './request'
+require 'pry'
+
+class Server
+
+  attr_reader :port
+
+  def initialize(port = nil)
+    @port = port || 2000
+  end
+
+  def listen
+    socket = TCPServer.open(port)
+    loop do
+      client = socket.accept
+      plain_request = []
+      while (line = client.readline)
+        plain_request << line
+      end
+      request = Request.parse(plain_request)
+      response = route request
+      client.puts response.to_s
+      client.close
+    end
+  end
+
+  def route(request)
+    case request.headers['Method']
+    when 'GET' then list_messages
+    when 'POST' then create_message(request.body)
+    end
+  end
+
+  def messages
+    @messages ||= []
+  end
+
+  def list_messages
+    Request.new.tap do |request|
+      request.headers = {'Status' => 200}
+      request.body = messages.join("\n")
+    end
+  end
+
+  def create_message(message)
+    messages << message
+    Request.new.tap do |request|
+      request.headers = {'Status' => 201}
+      request.body = ''
+    end
+  end
+end
+
+Server.new.listen
